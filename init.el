590ffccd7a08a98f06926b4ff8146f510b82ee18
Ben Hartmann <bhartmann@bens-mbp-2.localdomain>
2020-01-10 13:58:15 +0100

    Initial commit of cleaned up init.el

---
+++ init.el
@@ -0,0 +1,523 @@
+;; base emacs settings for operation
+(setq delete-old-versions -1 )		; delete excess backup versions silently
+(setq version-control t )		; use version control
+(setq vc-make-backup-files t )
+(setq vc-follow-symlinks t )
+(setq initial-scratch-message "Welcome in Emacs")
+
+
+;; clear unnecessary tool-, menu- and scrollbar
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (and (not (eq system-type 'darwin)) (fboundp 'menu-bar-mode))
+  (menu-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+
+(setq ns-use-srgb-colorspace nil)
+
+;; editor text stuff
+(setq coding-system-for-read 'utf-8 )	; use utf-8 by default
+(setq coding-system-for-write 'utf-8 )
+(setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
+(setq default-fill-column 80)
+(global-hl-line-mode +1)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(setq show-paren-style 'parenthesis)
+(show-paren-mode +1)
+
+;;; backups/saves 
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
+
+;;; Misc stuff
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq frame-title-format "%b %+%+ %f")
+
+;; recent file list
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+
+;;package loading and repos
+(require 'package)
+(setq load-prefer-newer t
+      package-enable-at-startup nil
+      package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("org" . "http://orgmode.org/elpa/")
+        ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-verbose t
+      use-package-always-ensure t) ;; makes sure thst every package is available
+
+;; osx fixes
+(when (eq system-type 'darwin)
+  (setq mac-command-key-is-meta nil)
+  (setq mac-command-modifier 'super)
+  (setq mac-option-key-is-meta t)
+  (setq mac-option-modifier 'meta)
+  (setq mac-right-option-modifier nil))
+
+;; Needs to move to os specific settings 
+(use-package osx-trash                 
+  :if (eq system-type 'darwin)
+  :ensure t
+  :init (osx-trash-setup))
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-one t)
+  (doom-themes-visual-bell-config))
+
+
+;; following list of packages and keybindings needs to be seperated into multiple files etc. to keep this file sane ;)
+;;; navigation
+
+;; ivy
+(use-package ivy
+  :ensure t
+  :diminish ivy-mode
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (ivy-mode 1)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (bind-key "C-c C-r" 'ivy-resume)
+  (load "~/.emacs.d/custom/ivy_buffer_extend.el")
+  (require 'ivy_buffer_extend))
+
+(use-package ranger
+  :ensure t
+  :commands (ranger)
+  :bind (("C-x d" . deer))
+  :config
+  (setq ranger-cleanup-eagerly t))
+
+(use-package winner
+  :ensure t
+  :defer t)
+
+(use-package popwin
+  :ensure t
+  :config (popwin-mode 1)) 
+
+(use-package smart-mode-line :ensure t)
+
+(use-package undo-tree
+  :ensure t
+  :diminish undo-tree-mode
+  :config
+  (progn
+    (global-undo-tree-mode)
+    (setq undo-tree-visualizer-timestamps t)
+    (setq undo-tree-visualizer-diff t)))
+
+;; Mover where I mean -> (part of better defaults)
+ (use-package mwim
+   :ensure t
+   :defer t
+   :init
+    (progn
+      (global-set-key (kbd "C-a") 'mwim-beginning-of-code-or-line)
+      (global-set-key (kbd "C-e") 'mwim-end-of-code-or-line)))
+
+;; for working project-based
+(use-package projectile
+  :ensure t
+  :diminish projectile-mode
+  :config
+  (setq projectile-enable-caching t
+        projectile-completion-system 'ivy)
+  (projectile-mode))
+
+(use-package counsel-projectile
+  :ensure t
+  :config
+  (counsel-projectile-mode))
+
+;; smooth scrolling
+(use-package smooth-scroll
+  :ensure t
+  :if (display-graphic-p)
+  :diminish smooth-scroll-mode
+  :config
+  (setq smooth-scroll/vscroll-step-size 8)
+  (smooth-scroll-mode))
+
+;; highlight changes
+(use-package git-gutter-fringe
+  :ensure t
+  :diminish git-gutter-mode
+  :init (global-git-gutter-mode))
+
+(use-package git-timemachine
+  :ensure t
+  :defer t)
+  
+;;; Searching
+(use-package ag
+  :commands (ag ag-files ag-regexp ag-project ag-dired helm-ag)
+  :config (setq ag-highlight-search t
+                ag-reuse-buffers t))
+
+;; autocomplete stuff
+(use-package company               
+  :ensure t
+  :defer t
+  :init (global-company-mode t)
+  :config
+    (setq company-tooltip-align-annotations t
+          company-idle-delay 0.2
+          ;; min prefix of 2 chars
+          company-minimum-prefix-length 2
+          company-require-match nil))
+
+;; Show help in tooltip
+(use-package company-quickhelp          
+    :ensure t
+    :defer t
+    :init (with-eval-after-load 'company
+            (company-quickhelp-mode)))
+
+(use-package company-box
+  :init
+  (progn
+    (add-hook 'company-mode-hook 'company-box-mode)))
+
+
+;; show possible commands in minibuffer
+(use-package which-key
+  :ensure t
+  :diminish which-key-mode
+  :config (which-key-mode))
+
+;; magit
+(use-package magit
+  :ensure t
+  :commands (magit-status projectile-vc)
+  :config
+  (add-to-list 'magit-log-arguments "--no-abbrev-commit")
+  (setq magit-popup-use-prefix-argument 'default
+        magit-completing-read-function 'ivy-completing-read))
+
+(global-git-commit-mode)
+
+;; make emacs aware of your shell path
+(use-package exec-path-from-shell
+  :ensure t
+  :if (not (eq system-type 'windows-nt))
+  :init (exec-path-from-shell-initialize))
+
+;; get linum right
+ (use-package nlinum
+    :init
+    (progn
+      (add-hook 'prog-mode-hook 'nlinum-mode)
+      (add-hook 'text-mode-hook 'nlinum-mode)
+      (setq nlinum-format "%4d")))
+
+;; snippets
+(use-package yasnippet
+  :ensure t
+  :config
+  (use-package yasnippet-snippets
+    :ensure t)
+  (yas-global-mode t)
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "C-'") #'yas-expand)
+  (add-to-list #'yas-snippet-dirs "my-personal-snippets")
+  (yas-reload-all)
+  (setq yas-prompt-functions '(yas-ido-prompt))
+  (defun help/yas-after-exit-snippet-hook-fn ()
+    (prettify-symbols-mode)
+    (prettify-symbols-mode))
+  (add-hook 'yas-after-exit-snippet-hook #'help/yas-after-exit-snippet-hook-fn)
+  :diminish yas-minor-mode)
+
+
+;; neotree
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         nil
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-width                         35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    (treemacs-resize-icons 18)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+;; Smartparens all the things
+(use-package smartparens
+  :ensure t
+  :diminish smartparens-mode
+  :config
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)))
+
+;; Nice colors for delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :defer t
+  :init
+  (progn
+    (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)))
+
+(use-package tramp
+  :ensure t
+  :config
+  (setq tramp-verbose 9
+        tramp-default-method "ssh"
+        tramp-ssh-controlmaster-options
+        (concat "-o ControlPath=/tmp/tramp.%%r@%%h:%%p "
+                "-o ControlMaster=auto "
+                "-o ControlPersist=no")))
+
+(use-package spaceline-config
+  :ensure spaceline
+  :config
+  (spaceline-emacs-theme))
+
+;; counsel
+(use-package counsel
+  :ensure t
+  :bind
+  ("M-x" . counsel-M-x)
+  ("C-c s" . counsel-ag))
+
+(use-package counsel-projectile :ensure t
+  :bind* (("C-P" . counsel-projectile-switch-to-buffer)
+          ("C-c p p" . counsel-projectile-switch-project))
+  :config
+  (counsel-projectile-mode))
+
+(use-package company-quickhelp  
+  :ensure t
+  :defer t
+  :init (add-hook 'global-company-mode-hook #'company-quickhelp-mode))
+
+(use-package hydra
+  :ensure t
+  :defer t
+  :init
+  (defhydra hydra-zoom (global-map "<f2>")
+    "zoom"
+    ("g" text-scale-increase)
+    ("l" text-scale-decrease)))
+
+(use-package clojure-mode
+  :ensure t
+  :config
+  (add-hook 'clojure-mode-hook #'smartparens-mode)
+  (add-hook 'clojure-mode-hook #'subword-mode) 
+  (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+  (add-hook 'clojure-mode-hook #'aggressive-indent-mode))
+
+(use-package cider
+  :ensure t
+  :config
+  (add-hook 'cider-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'eldoc-mode)
+  (add-hook 'cider-repl-mode-hook #'smartparens-mode)
+  (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode))
+
+(use-package swiper
+  :ensure t
+  :config (progn
+            (ivy-mode 1)
+            (setq ivy-use-virtual-buffers t)
+            (global-set-key "\C-s" 'swiper)
+            (global-set-key "\C-r" 'swiper)
+            (global-set-key (kbd "C-c C-r") 'ivy-resume)
+            (global-set-key [f6] 'ivy-resume) 
+            (setq ivy-display-style 'fancy)
+            (defun bjm-swiper-recenter (&rest args)
+              "recenter display after swiper"
+              (recenter))
+            (advice-add 'swiper :after #'bjm-swiper-recenter)))
+
+(use-package windmove
+  ;; :defer 4
+  :ensure t
+  :config
+  ;; use command key on Mac
+  (windmove-default-keybindings 'super)
+  ;; wrap around at edges
+  (setq windmove-wrap-around t))
+
+(use-package org
+  :ensure t
+  :config
+  (progn
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((clojure . t)
+       (emacs-lisp . t)
+       (shell . t)))
+    (setq org-babel-clojure-backend 'cider)))
+
+;; Presentations wit reveal.js
+(use-package ox-reveal
+ :ensure t)
+
+(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+(setq org-reveal-mathjax t)
+
+(use-package htmlize
+  :ensure t)
+
+(use-package aggressive-indent
+  :ensure t
+  :config
+  (global-aggressive-indent-mode))
+
+(use-package elpy
+    :init
+    (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+    :custom
+    (elpy-rpc-backend "jedi"))
+
+(use-package python
+  :ensure nil
+  :mode ("\\.py" . python-mode)
+  :config
+  (setq python-indent-offset 4)
+  (elpy-enable))
+
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+(use-package flycheck-color-mode-line
+  :ensure t
+  :config
+  (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+
+(use-package flycheck-pos-tip
+  :ensure t
+  :config
+  (eval-after-load 'flycheck
+    '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+(use-package flycheck-clojure
+  :ensure t
+  :config
+  (eval-after-load 'flycheck '(flycheck-clojure-setup)))
+
+;; Nice Dashboard when you start emacs
+(use-package dashboard
+  :config
+  (setq dashboard-banner-logo-title "Welcome my master")
+  (setq dashboard-items '((projects . 5)
+                          (bookmarks . 5)
+                          (recents  . 5)))
+  (dashboard-setup-startup-hook))
+
+;; Golang stuff
+(use-package go-mode
+  :ensure t)
+
+(use-package  go-eldoc)
+(use-package  go-autocomplete)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (hugsql-ghosts atom-one-dark-theme go-mode dired-subtree all-the-icons-dired dired-sidebar which-key use-package undo-tree spaceline smooth-scroll smartparens smart-mode-line ranger rainbow-delimiters popwin ox-reveal osx-trash nlinum neotree mwim magit hydra htmlize git-gutter-fringe focus exec-path-from-shell elpy dashboard darkroom counsel-projectile company-quickhelp cider ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
